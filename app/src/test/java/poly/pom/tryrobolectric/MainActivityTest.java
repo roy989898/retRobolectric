697418cf6de323690a4101c91cafc8d3e6436cb0
roy989898 <roy989898@gmail.com>
2016-08-29 12:41:14 +0800

    create test to test the app title

--- app/src/test/java/poly/pom/tryrobolectric/MainActivityTest.java
+++ app/src/test/java/poly/pom/tryrobolectric/MainActivityTest.java
@@ -2,24 +2,39 @@ package poly.pom.tryrobolectric;
 
 import android.os.Build;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.*;
 
 @RunWith(RobolectricGradleTestRunner.class)
-@Config(sdk = 23,constants = BuildConfig.class)
+@Config(sdk = 22,constants = BuildConfig.class)
 public class MainActivityTest {
 
 
+    private MainActivity activity;
+
+    @Before
+    public void setUp() throws Exception {
+
+         activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
     @Test
    public void shouldNotBeNull(){
-        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+        
         assertNotNull(activity);
 
     }
 
+    @Test
+    public void shouldHaveTitle() throws Exception {
+        assertTrue(activity.getTitle().toString().equals(RuntimeEnvironment.application.getString(R.string.app_name)));
+
+    }
 }
